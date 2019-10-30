1808fdb32e1b08679fd22ba4e205b8eca780612c
Joeun <kimgood91@gmail.com>
2019-10-30 18:50:59 +0900

    Set GlobalStyles and styled-comoponent theme

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Router from "./router";
+import GlobalStyles from "./GlobalStyles";
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router />
+      <GlobalStyles />
     </div>
   );
 };
