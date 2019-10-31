41ad8dddb27a2c4cba495c76855f1977ea0c6335
Joeun <kimgood91@gmail.com>
2019-11-01 00:43:47 +0900

    Test for rendering custom marker on kakaomap

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,16 +1,44 @@
-import * as React from "react";
-declare var daum: any;
+import React, { Component } from "react";
+import hollys from "../assets/marker/hollys-brandcolor.png";
+import tomtom from "../assets/marker/tomtom-brandcolor.png";
+declare var kakao: any;
 
 const mystyles = {
   width: "100%",
   height: "100vh"
 } as React.CSSProperties;
 
-class Map extends React.Component {
+class Map extends Component {
   componentDidMount() {
+    const hollysImageSrc = hollys;
+    const tomtomImageSrc = tomtom;
+    const imageSize = new kakao.maps.Size(57, 58);
+    const hollysMarkerImage = new kakao.maps.MarkerImage(
+      hollysImageSrc,
+      imageSize
+    );
+    const tomtomMarkerImage = new kakao.maps.MarkerImage(
+      tomtomImageSrc,
+      imageSize
+    );
+
     const el = document.getElementById("map");
-    let daumMap = new daum.maps.Map(el, {
-      center: new daum.maps.LatLng(33.450701, 126.570667)
+    let kakaoMap = new kakao.maps.Map(el, {
+      center: new kakao.maps.LatLng(33.450701, 126.570667)
+    });
+
+    const hollysMarker = new kakao.maps.Marker({
+      map: kakaoMap, // 마커를 표시할 지도
+      position: new kakao.maps.LatLng(33.450705, 126.570677),
+      title: "할리스", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image: hollysMarkerImage
+    });
+
+    const tomtomMarker = new kakao.maps.Marker({
+      map: kakaoMap, // 마커를 표시할 지도
+      position: new kakao.maps.LatLng(33.450936, 126.569477),
+      title: "탐앤탐스", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      image: tomtomMarkerImage
     });
   }
   render() {
