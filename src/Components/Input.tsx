ae90ee01eaa8c54dba94a61b65f277ba56c34a99
Joeun <kimgood91@gmail.com>
2019-10-31 18:19:40 +0900

    Add logo on input component

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import BtnHome from "../Components/btn-home";
+
+const SearchGroup = styled.fieldset`
+  display: block;
+  margin-inline-start: 2px;
+  margin-inline-end: 2px;
+  padding-block-start: 0.35em;
+  padding-inline-start: 0.75em;
+  padding-inline-end: 0.75em;
+  padding-block-end: 0.625em;
+  min-inline-size: min-content;
+`;
 
 const TextInput = styled.input`
   all: unset;
-  font-size: 16px;
-  width: 354px;
+  /* font-size: 16px; */
+  box-sizing: border-box;
+  padding: 10px 70px 10px 48px;
+  margin: 0;
+  width: 100%;
+  height: 51px;
+  text-shadow: "#000000";
+  /* border: solid 1px black; */
+  background-color: "#ffffff";
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  display: inline-block;
+  cursor: text;
+  font: 400 16px system-ui;
+  border-radius: ${props => props.theme.borderRadius};
 `;
 
 const FormPresenter = styled.form``;
@@ -20,13 +46,16 @@ interface IFormProps {
 
 export const Input: React.FC<IInputProps> = ({ value, onChange }) => {
   return (
-    <TextInput
-      type="text"
-      value={value}
-      onChange={onChange}
-      placeholder="장소, 지하철, 주소 검색"
-      // data-ga-event="search,input"
-    />
+    <>
+      <BtnHome />
+      <TextInput
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder="장소, 지하철, 주소 검색"
+        // data-ga-event="search,input"
+      />
+    </>
   );
 };
 
--- src/Routes/Search/SearchPresenter.tsx
+++ src/Routes/Search/SearchPresenter.tsx
@@ -5,7 +5,19 @@ import { Input, Form } from "../../Components/Input";
 import Helmet from "react-helmet";
 
 const Container = styled.div`
-  padding: 12px;
+  position: absolute;
+  top: 12px;
+  left: 11px;
+  width: 354px;
+  height: 152px;
+  border-radius: 3px;
+  border: 1px solid #cecece;
+  border-bottom: 1px solid #c0c0c0;
+  border-top: 1px solid #e9e9e9;
+  background-color: #fff;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.075), -1px 1px 1px rgba(0, 0, 0, 0.03),
+    1px 1px 1px rgba(0, 0, 0, 0.03);
+  z-index: 2;
 `;
 
 interface IProps {
@@ -34,12 +46,12 @@ const SearchPresenter: React.FC<IProps> = ({
     ) : (
       <>
         {result && (
-          <Container>
+          <div>
             <Helmet>
               <title>카페114 | 검색결과</title>
             </Helmet>
             Search
-          </Container>
+          </div>
         )}
       </>
     )}
