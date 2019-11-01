0812c52a55051df42aabf85aacbca192caba11e2
LeeYoungMin <miniyoung37@gmail.com>
2019-11-01 18:59:14 +0900

    map api zoom in/out test

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -94,11 +94,14 @@ class Map extends Component {
         infowindow.close();
       });
     }
-    function relayout() {
-      // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
-      // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다
-      // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
-      kakaoMap.relayout();
+    // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+    function zoomIn() {
+      kakaoMap.setLevel(kakaoMap.getLevel() - 1);
+    }
+
+    // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+    function zoomOut() {
+      kakaoMap.setLevel(kakaoMap.getLevel() + 1);
     }
   }
 
