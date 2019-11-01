756b2a0d6594fc55b9def1fad5de4245847621b0
Joeun <kimgood91@gmail.com>
2019-11-01 18:09:21 +0900

    Fix Home-button

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -10,7 +10,6 @@ interface IState {
   result: object;
 }
 export default class extends Component<{}, IState> {
-
   state = { term: "", result: {}, map: {}, error: "", loading: false };
 
   updateTerm = (event: React.ChangeEvent<HTMLInputElement>) => {
