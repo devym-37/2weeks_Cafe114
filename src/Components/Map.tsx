93101c4b3b7f9b8a8ee0c41c06f46531500facec
LeeYoungMin <miniyoung37@gmail.com>
2019-11-02 15:48:49 +0900

    Connect DB data and Mapping

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import hollys from "../assets/marker/hollys-brandcolor.png";
 import tomtom from "../assets/marker/tomtom-brandcolor.png";
-import { any } from "prop-types";
+import { serverApi } from "../Components/API";
 
 declare var kakao: any;
 
@@ -10,14 +10,37 @@ const mystyles = {
   height: "100vh"
 } as React.CSSProperties;
 
-const State = {
-  el: any,
-  kakaoMap: any
-};
-// default = 37.503469, 127.049782;
+interface Iinfo {
+  id: number;
+  name: string;
+  address: string;
+  telephone: string;
+  category: string;
+  createdAt: string;
+  detailCategory: string;
+  parkingLot: number;
+  smokingRoom: number;
+  updatedAt: string;
+}
 
-class Map extends Component {
-  componentDidMount() {
+interface Istate {
+  result: Array<Iinfo>;
+  loading: boolean;
+  error: string;
+}
+
+class Map extends Component<{}, Istate> {
+  state = { result: [], loading: true, error: "" };
+  async componentDidMount() {
+    try {
+      const { data: result } = await serverApi.getAllCafes();
+      this.setState({ result });
+    } catch {
+      this.setState({ error: "Can't load kakaoMap" });
+    } finally {
+      this.setState({ loading: false });
+    }
+
     const hollysImageSrc = hollys;
     const tomtomImageSrc = tomtom;
     const imageSize = new kakao.maps.Size(57, 58);
@@ -36,37 +59,22 @@ class Map extends Component {
       level: 5
     }); // 지도 생성
 
-    // const hollysMarker = new kakao.maps.Marker({
-    //   map: kakaoMap, // 마커를 표시할 지도
-    //   position: new kakao.maps.LatLng(33.450705, 126.570677),
-    //   title: "할리스", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-    //   image: hollysMarkerImage
-    // });
-
-    // const tomtomMarker = new kakao.maps.Marker({
-    //   map: kakaoMap, // 마커를 표시할 지도
-    //   position: new kakao.maps.LatLng(33.450936, 126.569477),
-    //   title: "탐앤탐스", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-    //   image: tomtomMarkerImage
-    // });
-
     this.marker(kakaoMap); // 위워크 marker
+    console.log("result ------", this.state.result);
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
 
-    // 장소 검색 객체를 생성합니다
-    var ps = new kakao.maps.services.Places(kakaoMap);
-    // 카테고리로 은행을 검색합니다
-    ps.keywordSearch("서울 할리스커피", placesSearchCB);
+    for (var i = 0; i < this.state.result.length; i++) {
+      geocoder.addressSearch(this.state.result[i].address, placesSearchCB);
+    }
+    // 주소로 좌표를 검색합니다
     function placesSearchCB(data: any, status: any, pagination: any) {
       if (status === kakao.maps.services.Status.OK) {
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
         var bounds = new kakao.maps.LatLngBounds();
-
-        for (var i = 0; i < data.length; i++) {
-          displayMarker(data[i]);
-          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }
-
+        displayMarker(data[0]);
+        bounds.extend(new kakao.maps.LatLng(data[0].y, data[0].x));
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
         kakaoMap.setBounds(bounds);
       }
@@ -84,21 +92,13 @@ class Map extends Component {
       kakao.maps.event.addListener(marker, "mouseover", function() {
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
         infowindow.setContent(
-          '<div style="padding:5px;font-size:12px;">' +
-            place.place_name +
-            "</div>"
+          '<div style="padding:5px;font-size:12px;">' + `cafe114` + "</div>"
         );
         infowindow.open(kakaoMap, marker);
       });
       kakao.maps.event.addListener(marker, "mouseout", function() {
         infowindow.close();
       });
-    }
-    function relayout() {
-      // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
-      // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다
-      // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
-      kakaoMap.relayout();
     }
   }
 
@@ -121,3 +121,11 @@ class Map extends Component {
   }
 }
 export default Map;
+
+// 마커 표시
+// const hollysMarker = new kakao.maps.Marker({
+//   map: kakaoMap, // 마커를 표시할 지도
+//   position: new kakao.maps.LatLng(33.450705, 126.570677),
+//   title: "할리스", // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+//   image: hollysMarkerImage
+// });
