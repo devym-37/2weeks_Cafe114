92f8818493b730f2bd7ae4cdf83600328c7c37eb
LeeYoungMin <miniyoung37@gmail.com>
2019-11-04 23:05:55 +0900

    navigator code rebuild Code

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -189,40 +189,69 @@ class Map extends Component<{}, Istate> {
       // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
       kakaoMap.panTo(moveLatLon);
     }
+
+    const COORDS = "coords";
     if (navigator.geolocation) {
-      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude, // 위도
-          lon = position.coords.longitude; // 경도
-
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-          message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
-      });
+      init(); // navigator 실행
     } else {
-      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      var locPosition = new kakao.maps.LatLng(37.503444, 127.049833),
-        message = "geolocation을 사용할수 없어요..";
-
-      displayMarker(locPosition, message);
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치
+      console.log("Cant access geo location");
+      this.marker(kakaoMap); // 위워크 marker
     }
     // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    function displayMarker(locPosition: any, message: any) {
+    function saveCoords(coordsObj: any) {
+      localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+    }
+
+    function handleGeoSucces(position: any) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const coordsObj = {
+        latitude,
+        longitude
+      };
+      var locPosition = new kakao.maps.LatLng(
+        coordsObj.latitude,
+        coordsObj.longitude
+      );
+      displayMarker(locPosition);
+      saveCoords(coordsObj);
+    }
+    function handleGeoError() {
+      console.log("Cant access geo location");
+    }
+    function askForCoords() {
+      navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+    }
+
+    function loadCoords() {
+      const loadCoords = localStorage.getItem(COORDS);
+      if (loadCoords === null) {
+        askForCoords();
+      } else {
+        const parseCoords = JSON.parse(loadCoords);
+        var locPosition = new kakao.maps.LatLng(
+          parseCoords.latitude,
+          parseCoords.longitude
+        );
+        displayMarker(locPosition);
+      }
+    }
+
+    function init() {
+      loadCoords();
+    }
+
+    function displayMarker(locPosition: any) {
       // 마커를 생성합니다
       var marker = new kakao.maps.Marker({
         map: kakaoMap,
         position: locPosition
       });
 
-      var iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true;
-
       // 인포윈도우를 생성합니다
       var infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
-        removable: iwRemoveable
+        content: "location"
       });
       // 인포윈도우를 마커위에 표시합니다
       infowindow.open(kakaoMap, marker);
@@ -252,7 +281,7 @@ class Map extends Component<{}, Istate> {
       image: codeMarkerImage
     });
     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-    kakao.maps.event.addListener(codeMarker, "click", function() {
+    kakao.maps.event.addListener(codeMarker, "mouseover", function() {
       // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
       infowindow.setContent(
         '<div class="customoverlay">' +
@@ -264,15 +293,18 @@ class Map extends Component<{}, Istate> {
       infowindow.open(map, codeMarker);
     });
     kakao.maps.event.addListener(codeMarker, "mouseout", function() {
-      // infowindow.close();
+      infowindow.close();
+    });
+    kakao.maps.event.addListener(codeMarker, "click", function() {
+      window.location.href = `https://www.codestates.com/`;
     });
     // 마커가 지도 위에 표시되도록 설정합니다
     codeMarker.setMap(map);
   }; // 위워크 marker
 
-  panTo = (map: any, y: string, x: string) => {
+  panTo = (map: any, position: any) => {
     // 이동할 위도 경도 위치를 생성합니다
-    var moveLatLon = new kakao.maps.LatLng(y, x);
+    var moveLatLon = position;
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
     map.panTo(moveLatLon);
